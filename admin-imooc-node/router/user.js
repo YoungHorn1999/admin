8f3567a8f4278e84bd26cbdfe6441cac209b050e
yuanhao05 <yuanhao05@baidu.com>
2021-02-23 15:57:43 +0800

    10-8生成JWT功能开发

--- admin-imooc-node/router/user.js
+++ admin-imooc-node/router/user.js
@@ -5,7 +5,7 @@ const { PWD_SALT, PRIVATE_KEY, JWT_EXPIRED } = require('../utils/constant')
 const { md5 } = require('../utils')
 const { body, validationResult } = require('express-validator')
 const boom = require('boom')
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const router = express.Router()
 
@@ -17,7 +17,7 @@ router.post(
     '/login',
     [
         body('username').isString().withMessage('用户名必须为字符'),
-        body('password').isNumeric().withMessage('密码必须为数字')
+        body('password').isString().withMessage('密码必须为字符')
     ],
     function (req, res, next) {
         const err = validationResult(req)
@@ -32,7 +32,12 @@ router.post(
                 if (!user || user.length === 0) {
                     new Result('登录失败').fail(res)
                 } else {
-                    new Result('登录成功').success(res)
+                    const token = jwt.sign(
+                        { username },
+                        PRIVATE_KEY,
+                        { expiresIn: JWT_EXPIRED }
+                    )
+                    new Result({ token }, '登录成功').success(res)
                 }
             })
         }
