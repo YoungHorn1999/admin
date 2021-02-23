4831c8866f5a4b9dd11db337fc196223214d971c
yuanhao05 <yuanhao05@baidu.com>
2021-02-23 14:00:17 +0800

    10-7表单验证

--- admin-imooc-node/router/user.js
+++ admin-imooc-node/router/user.js
@@ -3,8 +3,8 @@ const Result = require('../models/Result')
 const { login } = require('../services/user')
 const { PWD_SALT, PRIVATE_KEY, JWT_EXPIRED } = require('../utils/constant')
 const { md5 } = require('../utils')
-// const { body, validationResult } = require('express-validator')
-// const boom = require('boom')
+const { body, validationResult } = require('express-validator')
+const boom = require('boom')
 // const jwt = require('jsonwebtoken')
 
 const router = express.Router()
@@ -13,17 +13,30 @@ router.get('/info', function (req, res, next) {
     res.json('user info...')
 })
 
-router.post('/login', function (req, res) {
-    let { username, password } = req.body
-    password = md5(`${password}${PWD_SALT}`)
-
-    login(username, password).then(user => {
-        if (!user || user.length === 0) {
-            new Result('登录失败').fail(res)
+router.post(
+    '/login',
+    [
+        body('username').isString().withMessage('用户名必须为字符'),
+        body('password').isNumeric().withMessage('密码必须为数字')
+    ],
+    function (req, res, next) {
+        const err = validationResult(req)
+        if (!err.isEmpty()) {
+            const [{ msg }] = err.errors
+            next(boom.badRequest(msg))
         } else {
-            new Result('登录成功').success(res)
+            let { username, password } = req.body
+            password = md5(`${password}${PWD_SALT}`)
+
+            login(username, password).then(user => {
+                if (!user || user.length === 0) {
+                    new Result('登录失败').fail(res)
+                } else {
+                    new Result('登录成功').success(res)
+                }
+            })
         }
+
     })
-})
 
 module.exports = router
