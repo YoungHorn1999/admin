62ce81f8e859ed9803e86c225c5b422bc51b6104
yuanhao05 <yuanhao05@baidu.com>
2021-02-24 20:38:05 +0800

    11-5文件上传API开发

--- admin-imooc-node/utils/constant.js
+++ admin-imooc-node/utils/constant.js
@@ -1,3 +1,8 @@
+const { env } = require('./env')
+const UPLOAD_PATH = env === 'dev'
+    ? '/Users/yuanhao05/upload/admin-upload-ebook'
+    : '/root/upload/admin-upload/ebook'
+
 module.exports = {
     CODE_ERROR: -1,
     CODE_SUCCESS: 0,
@@ -6,4 +11,5 @@ module.exports = {
     PWD_SALT: 'admin_imooc_node',
     PRIVATE_KEY: 'admin_imooc_node_test_youbaobao_xyz',
     JWT_EXPIRED: 60 * 60, // token失效时间
+    UPLOAD_PATH
 }
