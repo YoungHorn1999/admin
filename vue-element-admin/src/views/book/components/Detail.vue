796add5d4abef4388c40cd981627a11339c2433c
yuanhao05 <yuanhao05@baidu.com>
2021-02-24 22:09:07 +0800

    11-7电子书表单开发

--- vue-element-admin/src/views/book/components/Detail.vue
+++ vue-element-admin/src/views/book/components/Detail.vue
@@ -15,9 +15,108 @@
       <el-row>
         <Warning />
         <el-col :span="24">
-          <ebook-upload />
+          <ebook-upload
+            :file-list="fileList"
+            :disabled="isEdit"
+            @onSuccess="onUploadSuccess"
+            @onRemove="onUploadRemove"
+          />
         </el-col>
-        <el-col :span="24" />
+        <el-col :span="24">
+          <el-form-item prop="title">
+            <MdInput
+              v-model="postForm.title"
+              :maxlength="100"
+              name="name"
+              required
+            >
+              书名
+            </MdInput>
+          </el-form-item>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="作者：" :label-width="labelWidth">
+                <el-input v-model="postForm.author" placeholder="作者" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="出版社：" :label-width="labelWidth">
+                <el-input v-model="postForm.publisher" placeholder="出版社" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="语言：" :label-width="labelWidth">
+                <el-input v-model="postForm.language" placeholder="语言" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="根文件：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.rootFile"
+                  placeholder="根文件"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="文件路径：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.filePath"
+                  placeholder="文件路径"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="解压路径：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.unzipPath"
+                  placeholder="解压路径"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="封面路径：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.filePath"
+                  placeholder="封面路径"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="文件名称：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.unzipPath"
+                  placeholder="文件名称"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="24">
+              <el-form-item label="封面：" :label-width="labelWidth">
+                <a v-if="postForm.cover" :href="postForm.cover" target="_blank">
+                  <img :src="postForm.cover" class="preview-img" />
+                </a>
+                <span v-else>无</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="目录：" :label-width="labelWidth">
+                <div
+                  v-if="postForm.contents && postForm.contents.length > 0"
+                  class="contents-wrapper"
+                >
+                  <el-tree />
+                </div>
+                <span v-else>无</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
       </el-row>
     </div>
   </el-form>
@@ -27,6 +126,7 @@
 import Sticky from "../../../components/Sticky/index";
 import Warning from "./Warning";
 import EbookUpload from "../../../components/EbookUpload";
+import MdInput from "../../../components/MDinput/index";
 
 export default {
   components: {
@@ -43,6 +143,8 @@ export default {
       postForm: {
         status: "draft",
       },
+      fileList: [],
+      labelWidth: "120px",
     };
   },
   methods: {
@@ -55,6 +157,12 @@ export default {
         this.loading = false;
       }, 1000);
     },
+    onUploadSuccess() {
+      console.log("onUploadSuccess");
+    },
+    onUploadRemove() {
+      console.log("onUploadRemove");
+    },
   },
 };
 </script>
@@ -62,5 +170,9 @@ export default {
 <style lang="scss" scoped>
 .detail-container {
   padding: 40px 50px 20px;
+  .preview-img {
+    width: 200px;
+    height: 270px;
+  }
 }
 </style>
