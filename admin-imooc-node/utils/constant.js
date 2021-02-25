0dceb775846b81a384bad9bacd382d27838cb376
yuanhao05 <yuanhao05@baidu.com>
2021-02-25 19:13:46 +0800

    12-2电子书book对象开发

--- admin-imooc-node/utils/constant.js
+++ admin-imooc-node/utils/constant.js
@@ -2,6 +2,7 @@ const { env } = require('./env')
 const UPLOAD_PATH = env === 'dev'
     ? '/Users/yuanhao05/upload/admin-upload-ebook'
     : '/root/upload/admin-upload/ebook'
+const UPLOAD_URL = 'http://localhost:8089'
 
 module.exports = {
     CODE_ERROR: -1,
@@ -11,5 +12,7 @@ module.exports = {
     PWD_SALT: 'admin_imooc_node',
     PRIVATE_KEY: 'admin_imooc_node_test_youbaobao_xyz',
     JWT_EXPIRED: 60 * 60, // token失效时间
-    UPLOAD_PATH
+    UPLOAD_PATH,
+    MIME_TYPE_EPUB: 'application/epub+zip',
+    UPLOAD_URL
 }
